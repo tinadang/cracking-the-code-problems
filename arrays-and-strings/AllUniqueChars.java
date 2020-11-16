26927801c38bf0e8b7b4ade154d8e4851743d240
Tina Dang <tinad3@uci.edu>
2020-11-16 15:54:12 -0800

    Fix spacing in file

--- arrays-and-strings/AllUniqueChars.java
+++ arrays-and-strings/AllUniqueChars.java
@@ -25,6 +25,7 @@ public class AllUniqueChars {
     */
 
     // public static boolean isUniqueChars(String str) {
+        
     //     boolean[] char_set = new boolean[256];
     //     for (int i=0; i < str.length(); i++) {
     //         int val = str.charAt(i);
