b5a5a03d4231e55618d808f5b428583f3c1638a3
Tina Dang <tinad3@uci.edu>
2020-11-19 20:27:40 -0800

    Add replace space problem

---
+++ arrays-and-strings/ReplaceSpace.java
@@ -0,0 +1,37 @@
+public class ReplaceSpace {
+    public static void main(String[] args) {
+        char[] test1 =  {'h', 'i', ' ', 't', 'h', 'e', 'r', 'e'};
+
+        replaceSpace(test1, 8);
+    }
+
+    public static void replaceSpace(char[] str, int length) {
+        int spaceCount = 0;
+        for (int i=0; i < length; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+        System.out.println(str);
+        int newLength = length + (spaceCount*2);
+        char[] newStr = new char[newLength];
+        
+        for (int i=length-1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                newStr[newLength-1] = '0';
+                newStr[newLength-2] = '2';
+                newStr[newLength-3] = '%';
+                newLength -= 3;
+            } else {
+                newStr[newLength - 1] = str[i];
+                newLength -= 1;
+            }
+        }
+
+        System.out.println(newStr);
+        
+
+
+
+    }
+}
