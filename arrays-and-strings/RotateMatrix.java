6b738ae5b3848cf84455f6aaa3a73e55e366bdcb
Tina Dang <tinad3@uci.edu>
2020-11-29 17:20:07 -0800

    add rotate matrix solution and setrow and column to zero solution

---
+++ arrays-and-strings/RotateMatrix.java
@@ -0,0 +1,53 @@
+public class RotateMatrix {
+    public static void main(String[] args) {
+        int[][] test1 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+
+        printTwoDimensionalArray(test1);
+        System.out.println("\n");
+        rotateMatrix(test1, 4);
+        printTwoDimensionalArray(test1);
+    }
+
+    public static void rotateMatrix(int[][] matrix, int n) {
+        for (int row=0; row < n/2; row++) {
+            // Obtains the last index (column) for the row
+            int last = n - 1 - row;
+            for (int col=row; col < last; col++) {
+                // Offset for positions
+                int offset = col - row;
+
+                // Saves the top left value
+                int leftTop = matrix[row][col];
+
+                // Sets the top left to the bottom left value
+                matrix[row][col] = matrix[last-offset][row];
+                
+                // Sets the bottom left to the right bottom value
+                matrix[last-offset][row] = matrix[last][last-offset];
+
+                // Sets the right bottom to the top right value
+                matrix[last][last-offset] = matrix[col][last];
+                
+                // Sets the top right to the the top left value
+                matrix[col][last] = leftTop;
+                
+                // System.out.println("Row #: " + row);
+                // System.out.println("Start index: " + col);
+                // System.out.println("Last index: " + last);
+                // System.out.println("Offset: " + offset);
+                // System.out.println("Last - offset: " + (last - offset) + "\n");
+            
+            }
+        }
+    }
+
+    public static void printTwoDimensionalArray(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf("%d ", a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ arrays-and-strings/SetRowAndColumnToZero.java
@@ -0,0 +1,44 @@
+public class SetRowAndColumnToZero {
+    public static void main(String[] args) {
+        int[][] test1 = {{1, 1, 1, 1}, {1, 0, 1, 1}, {1, 1, 0, 1}};
+
+        printTwoDimensionalArray(test1);
+        System.out.println();
+        setZeros(test1);
+        printTwoDimensionalArray(test1);
+    }
+
+
+    public static void setZeros(int[][] matrix) {
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+
+        for (int i=0; i < matrix.length; i++) {
+            for (int j=0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+
+        for (int i=0; i < matrix.length; i++) {
+            for (int j=0; j < matrix[0].length; j++) {
+                if (row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }         
+            }
+        }
+
+    }
+
+    public static void printTwoDimensionalArray(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf("%d ", a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
